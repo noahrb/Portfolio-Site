08ad514b0484a0b30adff960895743dc4e19289e
Noah Beverly <noahbeverly@gmail.com>
2019-06-18 17:15:03 -0600

    Addding navbar and some styling to the login-system

--- login-system/header.php
+++ login-system/header.php
@@ -5,7 +5,32 @@
         <meta name="description" content="This is an example of a meta description. This will often show up in search results">
         <meta name=viewport content="width=device-width, initial-scale=1">
         <title></title>
-        <!--<link rel="stylesheet" href="style.css">-->
+        <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <header>
+        <nav class="nav-header-main"></nav>>
+            <a class="header-logo" href="index.php">
+                <img src="img/logo.jpeg" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="header-login"></div>>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">Signup</a>
+            <form action="includes/logout.inc.php" method="post">
+                <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
+    </header>
     
