0149706ca171270436babdd4d7178f5bfc365cb6
Noah Beverly <noahbeverly@gmail.com>
2019-06-19 11:10:08 -0600

    Adding CSS, working header w/ resized logo
    Created icon size logo, added stylesheet, added classes to HTML.

--- login-system/header.php
+++ login-system/header.php
@@ -5,14 +5,13 @@
         <meta name="description" content="This is an example of a meta description. This will often show up in search results">
         <meta name=viewport content="width=device-width, initial-scale=1">
         <title></title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
     <header>
-        <nav class="nav-header-main"></nav>>
+        <nav class="nav-header-main">
             <a class="header-logo" href="index.php">
-                <img src="img/logo.jpeg" alt="logo">
+                <img src="img/icon.png" alt="logo">
             </a>
             <ul>
                 <li><a href="index.php">Home</a></li>
@@ -21,13 +20,13 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
-        <div class="header-login"></div>>
+        <div class="header-login">
             <form action="includes/login.inc.php" method="post">
                 <input type="text" name="mailuid" placeholder="Username/E-mail...">
                 <input type="password" name="pwd" placeholder="Password...">
                 <button type="submit" name="login-submit">Login</button>
             </form>
-            <a href="signup.php">Signup</a>
+            <a href="signup.php" class="header-signup">Signup</a>
             <form action="includes/logout.inc.php" method="post">
                 <button type="submit" name="logout-submit">Logout</button>
             </form>
